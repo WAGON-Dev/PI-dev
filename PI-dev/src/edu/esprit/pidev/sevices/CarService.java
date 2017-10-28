5adcb4eaff8f1f60817eca96cd51803943bac4a7
Ghassen <Ghassen@DESKTOP-2EAKT4F.mshome.net>
2017-10-29 00:40:35 +0200

    Merge origin/master
    Conflicts:
    	PI-dev/src/edu/esprit/pidev/gui/TestInterfaceCLient2.java

--- PI-dev/src/edu/esprit/pidev/sevices/CarService.java
+++ PI-dev/src/edu/esprit/pidev/sevices/CarService.java
@@ -131,6 +131,25 @@ public class CarService implements ICarService {
         
         return c;
     }
+    
+     public  List<Car> FindByRate(int max, int min) {
+        String query="SELECT * from voiture where rate>"+min+" and rate<"+max+""  ;
+        Car c = new Car();
+         List<Car> carList = new ArrayList<>();
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet result = statement.executeQuery(query);
+            while(result.next()){
+            c = new Car(result.getString(1),result.getString(2),result.getInt(3),result.getInt(4),result.getString(5),result.getBoolean(6));
+            carList.add(c);
+            }
+            
+        }catch(SQLException ex){
+        ex.printStackTrace();
+        }
+        
+        return carList;
+    }
 
     @Override
     public List<Car> getAll() {
